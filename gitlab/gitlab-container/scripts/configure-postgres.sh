551bd536db19bf49980aad25e345fc5eac37762f
Jochen Cordes <jcordes@redhat.com>
2016-05-31 16:00:53 +0200

    Using variable database-servicename now.!

--- gitlab/gitlab-container/scripts/configure-postgres.sh
+++ gitlab/gitlab-container/scripts/configure-postgres.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
+DATABASE_SERVICE_HOST=`echo "${DATABASE_SERVICE_NAME}_SERVICE_HOST" | tr "[:lower:]" "[:upper:]"`
+DATABASE_SERVICE_PORT=`echo "${DATABASE_SERVICE_NAME}_SERVICE_PORT" | tr "[:lower:]" "[:upper:]"`
+
 echo >> /etc/gitlab/gitlab.rb <<EOF
 gitlab_rails['db_adapter'] = "postgresql"
 gitlab_rails['db_encoding'] = "unicode"
 gitlab_rails['db_collation'] = nil
-gitlab_rails['db_database'] = "${POSTGRES_DB_NAME}"
+gitlab_rails['db_database'] = "${POSTGRESQL_DATABASE}"
 gitlab_rails['db_pool'] = 10
-gitlab_rails['db_username'] = "${POSTGRESQLUSER}"
+gitlab_rails['db_username'] = "${POSTGRESQL_USER}"
 gitlab_rails['db_password'] = "${POSTGRESQL_PASSWORD}"
-gitlab_rails['db_host'] = "${GITLAB_POSTGRES_SERVICE_HOST}"
-gitlab_rails['db_port'] = "${GITLAB_POSTGRES_SERVICE_PORT}"
+gitlab_rails['db_host'] = "${DATABASE_SERVICE_HOST}"
+gitlab_rails['db_port'] = "${DATABASE_SERVICE_PORT}"
 gitlab_rails['db_socket'] = nil
 gitlab_rails['db_sslmode'] = nil
 gitlab_rails['db_sslrootcert'] = nil
